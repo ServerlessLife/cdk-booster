04c0101e440dfdc4128132c980e69580a10c01ee
Marko (ServerlessLife) <marko@serverlesslife.com>
2025-04-09 07:41:27 +0200

    fix: wip

--- src/configuration/getConfigFromCliArgs.ts
+++ src/configuration/getConfigFromCliArgs.ts
@@ -14,6 +14,7 @@ export async function getConfigFromCliArgs(): Promise<LldConfigCliArgs> {
 
   program.name('lld').description('CDK Booster').version(version);
   program.option('-v, --verbose', 'Verbose logging');
+  program.arguments('<string>');
 
   program.parse(process.argv);
 
