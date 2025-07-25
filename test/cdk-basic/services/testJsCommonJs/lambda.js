e411c98ccb275e2d196c3a0f7380d295bc0158b5
Marko (ServerlessLife) <marko@serverlesslife.com>
2025-07-25 10:10:03 +0200

    Add test

--- test/cdk-basic/services/testJsCommonJs/lambda.js
+++ test/cdk-basic/services/testJsCommonJs/lambda.js
@@ -1,5 +1,6 @@
 const { STSClient, GetCallerIdentityCommand } = require('@aws-sdk/client-sts');
 const { v4: uuidv4 } = require('uuid');
+const { readFile } = require('fs/promises');
 
 const stsClient = new STSClient({});
 
@@ -8,12 +9,16 @@ exports.lambdaHandler = async () => {
   const command = new GetCallerIdentityCommand({});
   const identity = await stsClient.send(command);
 
+  // read the content of test.txt file
+  const testFileContent = await readFile('test.txt', 'utf-8');
+
   // check uuid works
   const uuid = uuidv4();
 
   const response = {
     accountId: identity.Account,
     testExternalLib: !!uuid,
+    testFileContent,
   };
 
   return response;
--- test/cdk-basic/services/testJsEsModule/lambda.js
+++ test/cdk-basic/services/testJsEsModule/lambda.js
@@ -1,4 +1,5 @@
 import { STSClient, GetCallerIdentityCommand } from '@aws-sdk/client-sts';
+import { readFile } from 'node:fs/promises';
 import { v4 as uuidv4 } from 'uuid';
 
 const stsClient = new STSClient({});
@@ -8,12 +9,16 @@ export const lambdaHandler = async () => {
   const command = new GetCallerIdentityCommand({});
   const identity = await stsClient.send(command);
 
+  // read the content of test.txt file
+  const testFileContent = await readFile('test.txt', 'utf-8');
+
   // check uuid works
   const uuid = uuidv4();
 
   const response = {
     accountId: identity.Account,
     testExternalLib: !!uuid,
+    testFileContent,
   };
 
   return response;
